959fedb11011e3e58cdbdbeafc19e6ac0958fc68
karthik40404 <karpython123@gmail.com>
2024-09-11 10:32:32 +0530

    abc

--- abstraction.py
+++ abstraction.py
@@ -1,23 +1,29 @@
-from abc import ABC,abstractmethod
-class animal(ABC):
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
     @abstractmethod
-    def make_sound(s):
-        print('sound')
-class bird (animal):
-    def fly (self):
+    def make_sound(self):
+        pass
+
+class Bird(Animal):
+    def fly(self):
         print('fly')
-    def make_sound(s):
+
+    def make_sound(self):
         print('bird sound')
-class cat (animal):
+
+class Cat(Animal):
     def run(self):
         print('run')
-    def make_sound(s):
+
+    def make_sound(self):
         print('cat sound')
 
-b1=bird()
+b1 = Bird()
 b1.fly()
 b1.make_sound()
 
 print('cat')
-c=cat()
+c = Cat()
 c.run()
+c.make_sound()
