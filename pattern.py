d5a6bd472d2fd297015ab12341861cc8ca67bdc2
karthik40404 <karpython123@gmail.com>
2024-10-26 01:58:36 +0530

    pattern*

---
+++ pattern.py
@@ -0,0 +1,25 @@
+# n=5
+# for i in range(n):   #rectangle
+#     for j in range(n):
+#         print('*' , end=' ')
+#     print()
+
+# n=5
+# for i in range(n):
+#     for j in range(i+1):   #increace triangle
+#         print('*' , end=" ")
+#     print()
+
+# n=5
+# for i in range(n):
+    # for j in range(i,n):    #decrease triangle
+#         print('*' , end=" ")
+#     print()
+
+# n = 5
+# for i in range(n):
+#     for j in range(i,n):
+#         print(" ", end=" ") #right triangle
+#     for j in range(i + 1):
+#         print("*", end=" ")
+#     print()
