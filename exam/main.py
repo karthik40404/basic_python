a308c5657f7536c313d0a21783073c48795adbca
karthik40404 <karpython123@gmail.com>
2024-10-26 10:54:01 +0530

    lib_exam

--- exam/main.py
+++ exam/main.py
@@ -1,4 +1,4 @@
-data = []
+data = [[1,'harry potter','j.k rowling',2000],[2,'Sapiens','Yuval Noah Harari',2010],[3,'amazing spiderman','stan lee',1985]]
 while True:
     print('\nWelcome to the Library Management System!')     
     print("1. Add books")
@@ -27,7 +27,7 @@ while True:
 
     elif option == 3:
         book_id_update = int(input("Enter the book ID to update: "))
-        book_found = False
+        book_found = 0
         for book in data:
             if book[0] == book_id_update:
                 print("Current Name: " + book[1])
@@ -43,34 +43,35 @@ while True:
                 book[3] = int(new_year) if new_year else book[3]
                 
                 print("Book updated successfully!\n")
-                book_found = True
+                book_found = 1
                 break        
         if not book_found:
             print("Book not found.")
 
     elif option == 4:
         book_id_remove = int(input("Enter the book ID to remove: "))
-        book_found = False
+        book_found = 0
         for book in data:
             if book[0] == book_id_remove:
                 data.remove(book)
-                book_found = True
+                book_found = 1
                 print(f"Book with ID {book_id_remove} has been removed.")
                 break
         if not book_found:
             print("Book not found.")
 
     elif option == 5:
-        search_name = input("Enter the name of the book to search: ").lower()
+        search_name = input("Enter the name of the book to search: ")
         found = False
         for book in data:
-            if search_name in book[1].lower():
+            if search_name in book[1]:
                 print(f"Found: Book ID: {book[0]}, Book Name: {book[1]}, Author: {book[2]}, Year of Publish: {book[3]}")
                 found = True
         if not found:
             print("Book not found.")
+
     elif option == 6:
-        print("Exiting the system...")
+        print("Exiting the system....")
         break
     else:
         print("Invalid option. Please try again.")
